3508cb35624c0934685b3ceab6b19520db70d6c1
LoistaAmanda <luluamandel@gmail.com>
2024-12-15 23:19:06 +0700

    push task pertemuan 10 backend

--- backend-pertemuan10/task.js
+++ backend-pertemuan10/task.js
@@ -2,37 +2,25 @@
  * Fungsi untuk menampilkan hasil download
  * @param {string} result - Nama file yang didownload
  */
-const showDownload = (result) => {
-  console.log("Download selesai");
-  console.log(`Hasil Download: ${result}`);
-};
+function showDownload(result) {
+  console.log("Download telah selesai");
+  console.log("Hasil dari Download: + result");
+}
 
 /**
  * Fungsi untuk download file
  * @param {function} callback - Function callback show
  */
-const download = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const result = "windows-10.exe";
-      resolve(result);
-    }, 3000);
-  });
-};
-
+function download(callShowDownload) {
+  setTimeout(function () {
+    const result = "windows-10.exe";
+    callShowDownload(result);
+  }, 3000);
+}
+download(showDownload);
 /**
  * TODO:
  * - Refactor callback ke Promise atau Async Await
  * - Refactor function ke ES6 Arrow Function
  * - Refactor string ke ES6 Template Literals
  */
-const handleDownload = async () => {
-  try {
-    const result = await download();
-    showDownload(result);
-  } catch (error) {
-    console.error("Terjadi kesalahan:", error);
-  }
-};
-
-handleDownload();
